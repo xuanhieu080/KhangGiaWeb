bbaf0488d5bdd16c10c0181358cc198bedb9a0c4
Hieu Phan Xuan <xuanhieu@192.168.1.6>
2024-01-26 21:31:59 +0700

    Update

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'GAK',
+            exec_mode: 'cluster',
+            instances: 'max', // Or a number of instances
+            script: '.output/server/index.mjs',
+            args: 'start',
+            port: 3002
+        }
+    ]
+}
