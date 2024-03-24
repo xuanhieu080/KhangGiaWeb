87032003a38a8d58056ed969a0523394752ca153
dunghm97 <dunghoang.5397@gmail.com>
2024-03-25 00:13:25 +0700

    feat: Update new blog page

--- components/articles/ArticleCard.vue
+++ components/articles/ArticleCard.vue
@@ -1,13 +1,61 @@
 <template>
-    <div>
-        Card
+    <div class="article-card w-full">
+        <div class="article-card__thumbnail w-full h-[400px] relative rounded-lg overflow-hidden">
+            <NuxtLink :to="article.link">
+                <img class="w-full h-full object-cover" :src="article.image" :alt="article.title" />
+            </NuxtLink>
+        </div>
+        <div class="article-card__content w-full py-4">
+            <div class="article-card__wrapper">
+                <div class="article-card__category mb-4 text-sm text-[#8e8e8e] font-semibold">
+                    <NuxtLink :to="article.category_link"> {{ article.category_name }} | {{ $t('Ngày đăng')  }}: {{ article.created_at }} </NuxtLink>
+                </div>
+                <h3 class="article-card__title text-[24px] font-bold mb-3">
+                    <NuxtLink :to="article.link" class="article-link">
+                        {{ article.title }}
+                    </NuxtLink>
+                </h3>
+                <div class="article-card__description" v-if="isDescription">
+                    <p>
+                        {{article.description}}
+                    </p>
+                </div>
+                <div class="article-card__viewcount text-[#8e8e8e] text-sm font-medium" v-if="isViewCount">{{ $t('Số lượt xem') }}: {{ article.view_count }}</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
 
+const prop = defineProps({
+    article: {
+        type: Object,
+        default: null,
+    },
+    isDescription: {
+        type: Boolean,
+        default: true,
+    },
+    isViewCount: {
+        type: Boolean,
+        default: true,
+    }
+});
 </script>
 
 <style lang="scss" scoped>
-
+@import '@/assets/scss/mixins.scss';
+.article-card {
+    .article-card_description {
+        p {
+            @include text-overflow(3);
+        }
+    }
+    .article-card__title {
+        .article-link {
+            @include text-overflow(2);
+        }
+    }
+}
 </style>
--- components/articles/ArticleSwiper.vue
+++ components/articles/ArticleSwiper.vue
@@ -1,7 +1,8 @@
 <template>
-    <div v-if="bannerList.length > 0" class="banner-wrapper relative">
+    <div class="swiper-container w-full">
+        <div class="title-swiper text-[28px] 2xl:text-[36px] font-bold mb-4">{{ title }}</div>
         <Swiper
-            ref="bannerSwiper"
+            ref="articleSwiper"
             :slidesPerView="1"
             :modules="modules"
             :loop="true"
@@ -10,26 +11,27 @@
                 disableOnInteraction: false,
             }"
             :navigation="{
-                nextEl: '.next-banner-btn',
-                prevEl: '.prev-banner-btn',
+                nextEl: '.next-article-btn',
+                prevEl: '.prev-article-btn',
+            }"
+            :pagination="{
+                clickable: true,
             }"
             :grabCursor="true"
-            class="swiper banner-swiper w-full h-full">
-            <SwiperSlide v-for="item in bannerList" :key="item">
-                <a href="https://www.coolmate.me/" target="_blank" rel="noopener noreferrer">
-                    <NuxtImg fit="contain" class="w-full h-full" loading="lazy" format="webp" :src="item.url" alt="" />
-                </a>
+            class="article-swiper w-full h-full">
+            <SwiperSlide v-for="item in articleList" :key="item">
+                <ArticleCard :article="item" />
             </SwiperSlide>
             <template v-slot:container-end>
-                <div class="navigation-btns flex flex-col absolute top-1/2 right-4 -translate-y-1/2 z-10">
-                    <UButton variant="ghost" color="none" size="lg" class="next-banner-btn">
+                <div class="navigation-btns flex flex-col absolute top-1/2 right-4 -translate-y-1/2 z-10" v-if="false">
+                    <UButton variant="ghost" color="none" size="lg" class="next-article-btn">
                         <svg width="27" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M20.4755 7.84696L12.961 0.33225C12.7465 0.117742 12.4606 0 12.1557 0C11.8505 0 11.5648 0.117912 11.3503 0.33225L10.668 1.01468C10.4537 1.22885 10.3356 1.51492 10.3356 1.81993C10.3356 2.12478 10.4537 2.42049 10.668 2.63466L15.0519 7.02818H1.12414C0.496176 7.02818 0 7.51979 0 8.14792V9.11269C0 9.74082 0.496176 10.282 1.12414 10.282H15.1017L10.6682 14.7C10.4539 14.9146 10.3358 15.1928 10.3358 15.4979C10.3358 15.8025 10.4539 16.0849 10.6682 16.2992L11.3505 16.9794C11.565 17.194 11.8507 17.3109 12.1559 17.3109C12.4607 17.3109 12.7466 17.1924 12.9611 16.9779L20.4757 9.46338C20.6907 9.2482 20.809 8.96095 20.8081 8.6556C20.8088 8.34923 20.6907 8.06181 20.4755 7.84696Z"
                                 fill="white"></path>
                         </svg>
                     </UButton>
-                    <UButton variant="ghost" color="none" size="lg" class="prev-banner-btn">
+                    <UButton variant="ghost" color="none" size="lg" class="prev-article-btn">
                         <svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M1.33258 10.7751L8.84713 18.2898C9.06163 18.5043 9.34753 18.6221 9.65238 18.6221C9.95756 18.6221 10.2433 18.5042 10.4578 18.2898L11.1401 17.6074C11.3544 17.3932 11.4725 17.1072 11.4725 16.8021C11.4725 16.4973 11.3544 16.2016 11.1401 15.9874L6.75618 11.5939L20.684 11.5939C21.3119 11.5939 21.8081 11.1023 21.8081 10.4742L21.8081 9.50938C21.8081 8.88125 21.3119 8.34007 20.684 8.34007L6.70645 8.34007L11.1399 3.92202C11.3542 3.70752 11.4723 3.42923 11.4723 3.12422C11.4723 2.81954 11.3542 2.5372 11.1399 2.32286L10.4576 1.64262C10.2431 1.42812 9.95739 1.31122 9.65221 1.31122C9.34736 1.31122 9.06146 1.42964 8.84696 1.64415L1.33241 9.15869C1.1174 9.37387 0.999148 9.66112 0.999994 9.96648C0.999317 10.2728 1.1174 10.5603 1.33258 10.7751Z"
@@ -41,25 +43,30 @@
         </Swiper>
     </div>
 </template>
+
 <script setup>
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/vue';
-import { Navigation, Autoplay } from 'swiper/modules';
+import { Navigation, Autoplay, Pagination } from 'swiper/modules';
 defineComponent({
     props: ['Swiper', 'SwiperSlide'],
 });
+import ArticleCard from '@@/components/articles/ArticleCard.vue';
 const props = defineProps({
-    bannerList: Array,
+    title: String,
+    articleList: Array,
     autoPlay: {
         type: Boolean,
-        default: false,
+        default: true,
     },
 });
-let modules = ref([Navigation]);
-const bannerSwiper = ref(null);
+
+let modules = ref([Navigation, Pagination]);
+const articleSwiper = ref(null);
 onBeforeMount(() => {
     if (props.autoPlay) {
         modules.value.push(Autoplay);
     }
 });
 </script>
+
 <style lang="scss" scoped></style>
