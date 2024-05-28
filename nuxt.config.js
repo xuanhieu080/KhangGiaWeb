4edbee1b41d631bc74156e23e722364e498fe029
dunghm97 <dunghoang.5397@gmail.com>
2024-05-28 14:35:20 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,17 +85,18 @@ export default defineNuxtConfig({
         // },
         locales: [
             {
-                code: 'en',
-                iso: 'en-US',
-                name: 'English',
-                file: 'en.json',
-            },
-            {
                 code: 'vi',
                 iso: 'vi-VN',
                 name: 'Việt Nam',
                 file: 'vi.json',
             },
+            {
+                code: 'en',
+                iso: 'en-US',
+                name: 'English',
+                file: 'en.json',
+            },
+            
         ],
         detectBrowserLanguage: false,
         defaultLocale: 'vi',
