3d5905a49624cf4dbed920ba4e59ed1a7e242e43
HieuPX <xuanhieu.ht96@gmail.com>
2024-07-27 14:55:18 +0700

    Update

--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -372,7 +372,7 @@
                     <h3 class="!text-2xl font-extrabold">Chi tiết sản phẩm</h3>
                     <div v-html="productItem.data?.description"></div>
                 </div>
-                <div v-if="!loadingProductHot && productHot" class="product-similar my-8">
+                <div v-if="!loadingProductHot && productHot.data" class="product-similar my-8">
                     <h3 class="w-full text-center !text-2xl md:!text-3xl !mb-8 font-extrabold">SẢN PHẨM BẠN CÓ THỂ THÍCH</h3>
                     <Swiper
                         :spaceBetween="0"
