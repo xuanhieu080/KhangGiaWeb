8dff1c6cc15b198272b617ce237d62b670b1c722
HieuPX <xuanhieu.ht96@gmail.com>
2026-07-01 11:26:31 +0700

    Update

--- layouts/main.vue
+++ layouts/main.vue
@@ -174,11 +174,6 @@ const alternateLocale = computed(() =>
 )
 
 useHead({
-    script: [
-        {
-            innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5MFK8NDW');`,
-        },
-    ],
     meta: [
         {
             name: 'location',
@@ -273,9 +268,9 @@ onMounted(() => {
     script.src = 'https://images.dmca.com/Badges/DMCABadgeHelper.min.js';
     document.body.appendChild(script);
 
-    const noscript = document.createElement('noscript');
-    noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MFK8NDW" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
-    document.body.prepend(noscript);
+    // const noscript = document.createElement('noscript');
+    // noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MFK8NDW" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+    // document.body.prepend(noscript);
 });
 </script>
 <style lang="scss" scoped>
